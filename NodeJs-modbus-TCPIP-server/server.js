648f203deb3bd791dbd1c4eb32ece9da4f2f75b9
Tóth János <jano9415@gmail.com>
2024-10-24 10:22:42 +0200

    NodeJs server is modified.

--- NodeJs-modbus-TCPIP-server/server.js
+++ NodeJs-modbus-TCPIP-server/server.js
@@ -15,10 +15,16 @@ server.on('postWriteMultipleRegisters', (value) => {
 
 let i = 0;
 
+//40005-ös regiszterbe írom a 8-as számot
+//server.holding.writeUInt16BE(13,8)
+//40006-os regiszterbe írom a 102-es számot
+//server.holding.writeUInt16BE(102,10)
+
+//Függvény, ami lefut minden másodpercben
 setInterval(() => {
     i++;
-    server.holding.writeUInt16BE(i,8) //40001 + 8/2 = 40005
-    server.holding.writeUInt16BE(i%3, 10) // 40006
+    server.holding.writeUInt16BE(i,8) //40001 + 8/2 = 40005, 40005-ös regiszterbe írom az i. számot
+    server.holding.writeUInt16BE(i%3, 10) // 40006, 40006-os regiszterbe írom az i mod 3 számot
     if(i >= 200) {i = 0}
 }, 1000);
 
